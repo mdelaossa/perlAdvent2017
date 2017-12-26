6676e50f5c298260307972bdec334934fad5bd6b
Mario de la Ossa <mariodelaossa@gmail.com>
2017-12-26 15:34:08 -0600

    Initial commit - puzzle loading harness

---
+++ lib/Advent.pm
@@ -0,0 +1,31 @@
+package Advent;
+use strict;
+use warnings FATAL => 'all';
+use Module::Load;
+use Try::Tiny;
+
+if ( @ARGV < 2 ) {
+    print "Please provide the day and puzzle number as arguments\n";
+    print "Example for Day 3, puzzle 2: ./advent.pl 3 2\n";
+    exit 1;
+}
+elsif ( $ARGV[1] > 2 ) {
+    print "There are only two puzzles per day\n";
+    exit 1;
+}
+
+my $day    = $ARGV[0];
+my $puzzle = $ARGV[1];
+
+try {
+    my $module_name = "Advent::Day$day";
+    load $module_name;
+    $module_name->solve($puzzle);
+}
+catch {
+    print "There was an error loading the solver for Day $day\n";
+    print "The solver might not exist\n";
+    exit 1;
+};
+
+1;
---
+++ lib/advent.pl
@@ -0,0 +1,7 @@
+#!/usr/local/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use File::Basename 'dirname';
+use lib dirname(__FILE__);
+use Advent;
+
